cb08177fd7cc47eaac8aca3e1c16d42e57d4e4bf
husseinbouik <116066389+husseinbouik@users.noreply.github.com>
2023-05-11 17:02:40 +0100

    informia

--- register.php
+++ register.php
@@ -4,15 +4,15 @@ require('connect.php');
 // check if the first name field is not empty
 if (!empty($_POST['first_name'])) {
     // hash the password
-    $hashed_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $hashed_password = password_hash($_POST['Password'], PASSWORD_DEFAULT);
 
     // prepare the SQL query to insert data into the Learner table
-    $stmt = $db->prepare("INSERT INTO Learner (learner_id, first_name, last_name, email, password) VALUES (NULL, :first_name, :last_name, :email, :password)");
+    $stmt = $db->prepare("INSERT INTO Learner ( first_name, last_name, email, password) VALUES (:first_name, :last_name, :email, :password)");
 
     // bind the form data to the prepared statement
     $stmt->bindParam(':first_name', $_POST['first_name']);
     $stmt->bindParam(':last_name', $_POST['last_name']);
-    $stmt->bindParam(':email', $_POST['email']);
+    $stmt->bindParam(':email', $_POST['Email']);
     $stmt->bindParam(':password', $hashed_password);
 
     // execute the prepared statement
@@ -25,5 +25,5 @@ if (!empty($_POST['first_name'])) {
 $db = null;
 
 // Redirect to the sign in page
-header("Location: signin.php");
+// header("Location: signin.php");
 ?>
